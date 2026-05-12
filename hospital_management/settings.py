593aaeab54756bcf6f913228f9015d938deacfb9
Rathapp <povrathapp@gmail.com>
2026-05-12 20:13:50 +0700

    update model

--- hospital_management/settings.py
+++ hospital_management/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'hospital_management.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'clinic_evening',
+        'USER':'root',
+        'PASSWORD':'root',
+        'HOST':'localhost',
+        'PORT':'3306'
+
     }
 }
 
--- patients/models.py
+++ patients/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class patient(models.Model):
+    first_name = models.CharField(max_length=100,null=True,blank=True)
+    last_name = models.CharField(max_length=100,null=True,blank=True)
+    sex = models.CharField(max_length=6,null=False,blank=False,choices={'male':'Male','female':'Female'})
+    dob = models.DateField(null=False,blank=False)
+    phone = models.CharField(max_length=10,null=True,blank=True)
+    address = models.CharField(max_length=150,null=True,blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated =models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    class Meta:
+        db_table = "patients"
+        ordering = ['-created']
+
+
